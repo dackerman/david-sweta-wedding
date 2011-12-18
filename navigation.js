0000c3254da223ad58a33f0ba23bddc8efc79aeb
dackerman <david.w.ackerman@gmail.com>
2011-12-18 09:18:31 -0800

    Added highlighting to the navigation bar

--- navigation.js
+++ navigation.js
@@ -1,52 +1,70 @@
-var navigation = {}
+var nav = {}
 
-navigation.elements = []; // elements that can be navigated to
+nav.navHighlightColor = "#533";
 
-navigation.initialize = function(element) {
-    navigation.collectIds(element, navigation.elements);
+nav.elements = []; // elements that can be navigated to
+
+nav.initialize = function(element) {
+    nav.collectIds(element, nav.elements);
 }
 
-navigation.collectIds = function(element, elementList) {
+nav.collectIds = function(element, elementList) {
     var children = element.childNodes[1].childNodes
 
     for (i = 0; i < children.length; i++) {
         if (children[i].id != "" && children[i].id != undefined) {
             elementList.push(children[i]);
-            navigation.setOnClick(children[i].id);
+            nav.setOnClick(children[i].id);
         }
     }
 };
 
-navigation.setOnClick = function(elementId) {
+nav.setOnClick = function(elementId) {
     document.getElementById(elementId).onclick = function() {
-        navigation.navigate(elementId);
+        nav.navigate(elementId);
     }
 }
 
-navigation.navigate = function(elementId) {
-    var elements = navigation.elements;
+nav.navigate = function(elementId) {
+    var elements = nav.elements;
+    nav.highlight(nav.elements, elementId,
+                  nav.setContentHidden, nav.setContentVisible);
+    nav.highlight(nav.elements, elementId,
+                  nav.setNavNormal, nav.setNavHighlight);
+
+}
+
+nav.highlight = function(elements, elementId, callback, highlightCallback) {
     for (i = 0; i < elements.length; i++) {
-        navigation.setContentHidden(elements[i]);
+        callback(elements[i]);
         if (elements[i].id == elementId) {
-            navigation.setContentVisible(elements[i]);
+            highlightCallback(elements[i]);
         }
     }
 }
 
-navigation.setContentVisible = function(navElement) {
-    var content = navigation.getContent(navElement.id);
+nav.setContentVisible = function(navElement) {
+    var content = nav.getContent(navElement.id);
     if (content != undefined) {
         content.style.display = "block";
     }
 }
 
-navigation.setContentHidden = function(navElement) {
-    var content = navigation.getContent(navElement.id);
+nav.setContentHidden = function(navElement) {
+    var content = nav.getContent(navElement.id);
     if (content != undefined) {
         content.style.display = "none";
     }
 }
 
-navigation.getContent = function(navElementId) {
+nav.setNavNormal = function(element) {
+    element.style.removeProperty("background-color");
+}
+
+nav.setNavHighlight = function(element) {
+    element.style.backgroundColor = nav.navHighlightColor;
+}
+
+nav.getContent = function(navElementId) {
     return document.getElementById("content_" + navElementId);
 }
