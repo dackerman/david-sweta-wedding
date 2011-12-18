7dd96226b11fbcbdd73e3be7874a2f996f83bd5f
dackerman <david.w.ackerman@gmail.com>
2011-12-18 00:13:46 -0800

    Javascript now working to show different content

--- navigation.js
+++ navigation.js
@@ -12,6 +12,41 @@ navigation.collectIds = function(element, elementList) {
     for (i = 0; i < children.length; i++) {
         if (children[i].id != "" && children[i].id != undefined) {
             elementList.push(children[i]);
+            navigation.setOnClick(children[i].id);
         }
     }
+};
+
+navigation.setOnClick = function(elementId) {
+    document.getElementById(elementId).onclick = function() {
+        navigation.navigate(elementId);
+    }
+}
+
+navigation.navigate = function(elementId) {
+    var elements = navigation.elements;
+    for (i = 0; i < elements.length; i++) {
+        navigation.setContentHidden(elements[i]);
+        if (elements[i].id == elementId) {
+            navigation.setContentVisible(elements[i]);
+        }
+    }
+}
+
+navigation.setContentVisible = function(navElement) {
+    var content = navigation.getContent(navElement.id);
+    if (content != undefined) {
+        content.style.display = "block";
+    }
+}
+
+navigation.setContentHidden = function(navElement) {
+    var content = navigation.getContent(navElement.id);
+    if (content != undefined) {
+        content.style.display = "none";
+    }
+}
+
+navigation.getContent = function(navElementId) {
+    return document.getElementById("content_" + navElementId);
 }
