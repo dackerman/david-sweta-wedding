c4f8223b2ad14c899a805baa912b429b90429776
David Ackerman <david.w.ackerman@gmail.com>
2013-07-01 21:13:12 -0700

    Updates site with post-wedding photos
    Adds a photos tab, removes some obsolete tabs, and adds a little counter
    for how many days we've been married

--- navigation.js
+++ navigation.js
@@ -1,4 +1,4 @@
-var nav = {}
+var nav = {};
 
 nav.navHighlightColor = "#ddd";
 
@@ -6,13 +6,13 @@ nav.elements = []; // elements that can be navigated to
 
 nav.initialize = function(element) {
     nav.collectIds(element, nav.elements);
-}
+};
 
 nav.collectIds = function(element, elementList) {
-    var children = element.childNodes[1].childNodes
+    var children = element.childNodes[1].childNodes;
 
     for (i = 0; i < children.length; i++) {
-        if (children[i].id != "" && children[i].id != undefined) {
+        if (children[i].id !== "" && children[i].id !== undefined) {
             elementList.push(children[i]);
             nav.setOnClick(children[i].id);
         }
@@ -22,13 +22,13 @@ nav.collectIds = function(element, elementList) {
 nav.setOnClick = function(elementId) {
     document.getElementById(elementId).onclick = function() {
         nav.navigate(elementId);
-    }
-}
+    };
+};
 
 nav.navigate = function(elementId) {
     var elements = nav.elements;
     window.location.hash = "#" + elementId;
-}
+};
 
 nav.onHashChange = function() {
     var elementId = window.location.hash.slice(1);
@@ -36,7 +36,7 @@ nav.onHashChange = function() {
                   nav.setContentHidden, nav.setContentVisible);
     nav.highlight(nav.elements, elementId,
                   nav.setNavNormal, nav.setNavHighlight);
-}
+};
 
 nav.highlight = function(elements, elementId, callback, highlightCallback) {
     for (i = 0; i < elements.length; i++) {
@@ -45,30 +45,38 @@ nav.highlight = function(elements, elementId, callback, highlightCallback) {
             highlightCallback(elements[i]);
         }
     }
-}
+};
 
 nav.setContentVisible = function(navElement) {
     var content = nav.getContent(navElement.id);
-    if (content != undefined) {
+    if (content !== undefined) {
         content.style.display = "block";
     }
-}
+};
 
 nav.setContentHidden = function(navElement) {
     var content = nav.getContent(navElement.id);
-    if (content != undefined) {
+    if (content !== undefined) {
         content.style.display = "none";
     }
-}
+};
 
 nav.setNavNormal = function(element) {
     element.style.removeProperty("background-color");
-}
+};
 
 nav.setNavHighlight = function(element) {
     element.style.backgroundColor = nav.navHighlightColor;
-}
+};
 
 nav.getContent = function(navElementId) {
     return document.getElementById("content_" + navElementId);
-}
+};
+
+var daysSinceWedding = function(element) {
+    var wedding = moment([2013, 5, 15]);
+    var today = moment();
+    var numDays = today.diff(wedding, 'days');
+
+    element.innerHTML = "(<em>happily married for " + numDays + " days!</em>)";
+};
